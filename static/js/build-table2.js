9da137e959d35351f167c98bfcfc3deffae585d1
ajay-d <ajay@deonarine.com>
2023-12-13 10:55:41 -0500

    submodules

---
+++ static/js/build-table2.js
@@ -0,0 +1,24 @@
+let mountains = [
+    { name: "Monte Falco", height: 1658, place: "Parco Foreste Casentinesi" },
+    { name: "Monte Falterona", height: 1654, place: "Parco Foreste Casentinesi" },
+    { name: "Poggio Scali", height: 1520, place: "Parco Foreste Casentinesi" },
+    { name: "Pratomagno", height: 1592, place: "Parco Foreste Casentinesi" },
+    { name: "Monte Amiata", height: 1738, place: "Siena" }
+    ];
+function generateTable(table, data) {
+    console.log("Ajay" + table);
+    for (let element of data) {
+        let row = table.insertRow();
+        for (key in element) {
+            let cell = row.insertCell();
+            let text = document.createTextNode(element[key]);
+            cell.appendChild(text);
+        }
+    }
+}
+
+var tableAjay = document.getElementById('tableauto');
+let data = Object.keys(mountains[0]);
+console.log("Ajay" + tableAjay);
+console.log("Ajay" + data);
+generateTable(tableAjay, mountains);
